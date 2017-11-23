36db091024ce54fda16bf970050c2dc0b5164798
Martín Loguancio <martin.loguancio@gmail.com>
2017-11-23 15:19:53 -0300

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,237 @@
+$(document).ready(function(){
+    //Canvas stuff
+    var canvas = $("#canvas")[0];
+    var ctx = canvas.getContext("2d");
+    var w = $(canvas).width();
+    var h = $(canvas).height();
+
+    //Lets save the cell width in a variable for easy control
+    var side = 4;
+    var board;
+    var cw = Math.round(w/side);
+    var win = false;
+    var winstring;
+    var enabled = true;
+
+    function BoardSlot(x,y,board) {
+        this.board = board;
+        this.x = x;
+        this.y = y;
+        this.piece = null;
+
+        this.set_piece = function(aPiece) {
+            this.piece = aPiece;
+            this.piece.update_board_slot(this);
+        }
+
+        this.is_space_now = function() {
+            this.board.set_space_slot(this);
+        }
+
+        this.render = function() {
+            this.piece.render(this.x,this.y);
+        }
+    }
+
+    function Piece (number,color,space) {
+        this.number = number;
+        this.color = color;
+        this.space = space;
+        this.slot = null;
+        
+        this.update_board_slot = function(aSlot) {
+            this.slot = aSlot;
+            if (this.space) this.slot.is_space_now();
+        }
+
+        this.render = function(x,y) {
+
+            ctx.fillStyle = this.color;
+            ctx.fillRect(x*cw, y*cw, cw, cw);
+            ctx.strokeStyle = "white";
+            ctx.strokeRect(x*cw, y*cw, cw, cw);
+
+            ctx.fillStyle = "black";
+            ctx.font=  Math.round(cw / 3) + "px Arial";
+            ctx.fillText(this.number, (x*cw) + Math.round(cw / 3), (y*cw) + Math.round(cw/3));
+        }
+
+    }
+
+    function Board(side) {
+        this.side = side;
+        this.slots = [];
+        this.space_slot = null;
+
+        this.set_space_slot = function(slot) {
+            this.space_slot = slot;
+        }
+
+
+        this.createPiece = function(x,y){
+            if(x==y && x == (this.side -1))
+                {
+                return new Piece((this.side*x) + y,"red",true);    
+                }
+            else
+             {
+                return new Piece((this.side*x) + y,"green",false);
+             }
+            
+        }
+
+        this.init = function() {
+            for(var i = 0; i < this.side; i++)
+            {
+                for(var j = 0; j < this.side; j++)
+                {
+                    var slot = new BoardSlot(i,j,this);
+                    var piece = this.createPiece(i,j);
+                    slot.set_piece(piece);
+                    this.slots.push(slot);
+                }
+            }
+
+            this.shuffle();
+        }
+
+        this.shuffle = function() {
+
+            for (var i = 0; i < this.slots.length; i++) {
+                var random_idx = i + Math.round(Math.random() * (this.slots.length-(i+1)))
+
+                var aux = this.slots[i].piece;
+                this.slots[i].set_piece(this.slots[random_idx].piece);
+                this.slots[random_idx].set_piece(aux);
+            }
+
+            if (this.solvable()) {
+                return;
+            } else {
+                console.log("Not Solvable");
+                this.shuffle();
+            }
+        }
+
+
+        this.solvable = function() {
+            
+            var inversions_count = this.get_inversions(this.slots);
+            var e = this.space_slot.y + 1;
+            return ((inversions_count + e) % 2) == (this.side % 2);
+
+        }
+
+        this.get_inversions = function (slot_array) {
+            var inversions_count = 0;
+            for (var i = 0; i < slot_array.length; i++) {
+                var actual_item = slot_array[i];
+                var permutations = slot_array.filter(function(item,index) {
+                    return index > i && item.piece.number < actual_item.piece.number;
+                });
+                inversions_count += permutations.length;
+            };
+            return inversions_count;
+        }
+
+        this.render = function() {
+
+            for(var i = 0; i < this.slots.length; i++)
+            {
+                this.slots[i].render();
+            }
+        }
+
+        this.idx_slot_at = function(x,y) {
+            return (x*this.side) + y;
+        }
+
+        this.switch_with = function(direction)
+        {
+            var direction_to_move = direction.mov(this.space_slot);
+
+            if (direction_to_move.x < 0 || direction_to_move.y < 0 || direction_to_move.x >= this.side || direction_to_move.y >= this.side) { return; }
+
+            var idx_to_move = this.idx_slot_at(direction_to_move.x,direction_to_move.y);
+            var idx_space = this.idx_slot_at(this.space_slot.x,this.space_slot.y);
+
+            var aux = this.slots[idx_to_move].piece;
+            this.slots[idx_to_move].set_piece(this.slots[idx_space].piece);
+            this.slots[idx_space].set_piece(aux);
+
+            this.check_win();
+        }
+
+        this.check_win = function (argument) {
+            var winning = true;
+            for (var i = 0; i < this.slots.length; i++) {
+                if (winning) {
+                    winning = this.slots[i].piece.number == i;    
+                }
+            };
+            win = winning;
+        }
+    }
+
+  //directions
+    var Left = { mov: function(slot) {  return { x: slot.x -1, y: slot.y } } };
+    var Up = { mov: function(slot) {  return { x: slot.x, y: slot.y -1 } } };
+    var Right = { mov: function(slot) {  return { x: slot.x +1, y: slot.y } } };
+    var Down = { mov: function(slot) {  return { x: slot.x , y: slot.y +1 } } };
+   
+    function paint()
+    {
+        //We need to paint the BG on every frame
+        //Lets paint the canvas now
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, w, h);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(0, 0, w, h);
+
+        if(win)
+        {
+            alert("Ganaste");
+            win = false;
+            enabled = false;
+        }
+        
+        board.render();    
+        
+
+        
+    }
+
+
+    $("#restart").on("click",function(e) {
+            init();
+            return;
+        });
+
+    
+
+    function init()
+    {
+        win = false;
+        enabled = true;
+        board = new Board(side);
+        board.init();
+
+        if(typeof game_loop != "undefined") clearInterval(game_loop);
+        game_loop = setInterval(paint, 60);
+    }
+
+
+    //Lets add the keyboard controls now
+    $(document).keydown(function(e){
+        if  (!enabled) {return;}
+        var key = e.which;
+        //We will add another clause to prevent reverse gear
+        if(key == "37") board.switch_with(Left);
+        else if(key == "38") board.switch_with(Up);
+        else if(key == "39") board.switch_with(Right);
+        else if(key == "40") board.switch_with(Down);
+        //The snake is now keyboard controllable
+    });
+
+    init();
+});
